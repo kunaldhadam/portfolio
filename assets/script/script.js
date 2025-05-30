b02b637f0fbba9b9d4b836bda0f0432a2b7fad21
Kunal <kunaldhadamkd@gmail.com>
2025-05-30 19:08:44 +0530

    new commit

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,8 +1,15 @@
 document.addEventListener("DOMContentLoaded", ()=>{
     const toggle = document.getElementById("menu-toggle");
     const mobileMenu = document.getElementById("mobile-menu");
+    const mobileMenuLink = document.querySelectorAll("#mobile-menu li");
 
     toggle.addEventListener("click", () => {
-      mobileMenu.classList.toggle("hidden");
+      mobileMenu.classList.toggle("hidden-menu");
     });
+
+    mobileMenuLink.forEach( link =>{
+      link.addEventListener("click", () =>{
+        mobileMenu.classList.add("hidden-menu");
+      })
+    })
 });
